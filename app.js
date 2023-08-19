cfeb8c51b8e002b5f31d24ecdada65e9935332e0
adogbeji <baka_1993@yahoo.co.uk>
2023-08-19 15:32:23 +0100

    Start adding middleware

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// For Next Time: Start adding Middleware (Lesson 60)
+// For Next Time: Start adding Middleware (Lesson 60 - 2:32)
 
 const app = express();
 
@@ -11,5 +11,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {});
 
 app.listen(3000);
