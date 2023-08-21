bb27a9c270901d4479ec4103731f8136b4560ba5
adogbeji <baka_1993@yahoo.co.uk>
2023-08-21 18:20:32 +0100

    Call next() function

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// For Next Time: Start adding Middleware (Lesson 60 - 2:32)
+// For Next Time: Start adding Middleware (Lesson 60 - 4:49)
 
 const app = express();
 
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     console.log('Middleware 1');
+    next();
 });
 
 app.use((req, res, next) => {
