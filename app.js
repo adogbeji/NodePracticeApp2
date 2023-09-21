adc79f424ddcbc5df786b35e47d968c12dad5c8f
adogbeji <baka_1993@yahoo.co.uk>
2023-09-21 16:14:12 +0100

    Add second middleware

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// For Next Time: Continue Handling Different Routes (Lesson 63 - 2:00)
+// For Next Time: Continue Handling Different Routes (Lesson 63 - 2:32)
 
 const app = express();
 
@@ -12,10 +12,9 @@ app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use((req, res, next) => {
-//     console.log('Middleware 1');
-//     next();
-// });
+app.use('/add-product', (req, res, next) => {
+    res.send('<h1>Add Product Page!</h1>');
+});
 
 app.use('/', (req, res, next) => {
     console.log('Middleware 2');
